6f0d1649ee5a22cab0b6f421e9173894dec4ce91
Nvidiaa_alex <alcasga2@epsg.upv.es>
2021-05-07 13:14:32 +0200

    .

--- Pruebas/receiver.py
+++ Pruebas/receiver.py
@@ -1,6 +1,6 @@
 import socket
 
-UDP_IP = "192.168.0.191"
+UDP_IP = ""
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, # Internet
@@ -8,9 +8,9 @@ sock = socket.socket(socket.AF_INET, # Internet
 sock.bind(("", UDP_PORT))
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print("received message: %s" % data)
-
+    data, addr = sock.recvfrom(1024)
+    message= json.loads(data.decode())
+    print("speed: %s"% message.get("speed")+" angle: %s"% message.get("angle"))
 
 
 
