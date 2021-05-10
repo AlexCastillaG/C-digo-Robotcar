d99bceb9407988415dc07280d3d080a9fb3958d0
Nvidiaa_alex <alcasga2@epsg.upv.es>
2021-05-10 13:20:08 +0200

    RX_Nvidia_updated

--- Nvidia/Receptor/nvidia_receiver_wifi.py
+++ Nvidia/Receptor/nvidia_receiver_wifi.py
@@ -13,26 +13,30 @@ sock= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
 #connections, vars and pins
-
-
+kit = ServoKit(channels=16)
+kit.servo[0].set_pulse_width_range[1000,2000]
 
 
 #defs
 
 
-#def initialization():
+def initialization():
 
-
+    kit.servo[0].angle=0
+    kit.servo[0].angle=180
+    kit.servo[0].angle=0
+    kit.servo[0].angle=90
 
 
 #main
 
-#initialization()
+initialization()
 
 while True:
     data, addr = sock.recvfrom(1024)
     message= json.loads(data.decode())
     print("speed: %s"% message.get("speed")+" angle: %s"% message.get("angle"))
+    kit.servo[0].angle=message.get("speed")
     ###############
 
 
