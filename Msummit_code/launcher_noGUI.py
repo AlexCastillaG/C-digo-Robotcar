a7ba6c611e9a3562bca7763680ba45772188dcb3
AlexCastillaG <alcasga2@epsg.upv.es>
2022-06-03 15:39:44 +0200

    host need to be fixed, it doenst accept comms after disconnection from wifi

--- Msummit_code/launcher_noGUI.py
+++ Msummit_code/launcher_noGUI.py
@@ -10,9 +10,11 @@ throttle_port = 1
 brake_port = 2
 wheel_port = 0
 
-
 PS4 = wheel_module.PS4Controller(usb_port,deadman_port,speedup_port,speeddown_port,throttle_port,brake_port,wheel_port)
 while True:
-    time.sleep(0.01)
-    PS4.move_car()
-    #PS4.mapping_tool()
+    try:
+        PS4.move_car()
+        #PS4.mapping_tool()
+    except OSError:
+        PS4 = wheel_module.PS4Controller(usb_port,deadman_port,speedup_port,speeddown_port,throttle_port,brake_port,wheel_port)
+        continue
